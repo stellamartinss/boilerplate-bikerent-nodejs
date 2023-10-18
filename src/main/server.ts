c05caecd23c852e461180481786826a6e444c7f4
stellamartinscrp <stella@edujob.comls>
2023-10-18 19:46:27 -0300

    wip

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,6 @@
 require('dotenv').config();
 import app from '@/main/config/app';
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 5001, () => {
   console.log('Server running...');
 });
